40de4e51b4fc635cbeb62bacf4be7593f046ac5e
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-10-11 12:35:39 -0300

    Create AtD20.py

---
+++ FUNCTIONS/AtD20.py
@@ -0,0 +1,14 @@
+"""void AtD20(int *dadosmatriz, int qntdados) {
+    for (int i = 0; i < qntdados; i++) {
+        dadosmatriz[i] = (rand() % 20) + 1;
+    }
+}"""
+import random
+
+def AtD20(qntDados):
+    dadosMatriz = []
+    for i in range (0, qntDados):
+        dadosMatriz.append(random.randint(0, 20))
+    return dadosMatriz
+
+print(AtD20(6))
