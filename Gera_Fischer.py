25c2852411b2ab9dcec7fac07e1dd6d0e278fa2b
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-10-16 12:25:25 -0300

    Update Gera_Fischer.py

--- Gera_Fischer.py
+++ Gera_Fischer.py
@@ -1,1 +1,23 @@
 
+from FUNCTIONS.Gera.Atributos import atribuir_atributo, modificadores
+from FUNCTIONS.Gera.Dados import rolar_dados
+from FUNCTIONS.Gera.Personagem import criar_personagem
+from FUNCTIONS.Gera.Tutorial import tutoriais
+from FUNCTIONS.Gera.Vida import pv
+
+from FUNCTIONS.Login.Cadastro_login import CadLog
+from FUNCTIONS.Login.menu import Menu
+from FUNCTIONS.Login.Validacao import validacao
+
+# Menu principal do Gera Fichar
+while True:
+    resposta = Menu.menu()
+    match resposta:
+        case 1:
+            usuarios = CadLog.cadastrar()
+        case 2:
+            if CadLog.logar(usuarios):
+                print ('Feito')
+        case 3:
+            print('obrigado por testar')
+            break
