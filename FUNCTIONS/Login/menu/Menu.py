a4f004d10f1a9c5a852972da104c61b4bcc0771b
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-10-16 13:35:39 -0300

    Update Menu.py

--- FUNCTIONS/Login/menu/Menu.py
+++ FUNCTIONS/Login/menu/Menu.py
@@ -1,12 +1,13 @@
 #Validação da opçao do menu
+import os
 def intinput():
     while True:
         try:
-            resp = int(input('Digite a opção no qual deseja realizar: '))
+            resp = int(input('\nDigite a opção no qual deseja realizar: '))
             if 1 <= resp <= 3:
                 return resp
             else:
-                print("Digite um número válido entre 1 e 3.")
+                print("\nDigite um número válido entre 1 e 3.")
                 print('1 - Realizar Cadastro')
                 print('2 - Login')
                 print('3 - Sair')
@@ -19,6 +20,8 @@ def intinput():
 #menu padrao do Gera
 def menu():
     while True:
+        os.system('cls')
+
         print("\nSeu organizador e gerenciador de Ficha do personagem\n")
         print('1 - Realizar Cadastro')
         print('2 - Login')
