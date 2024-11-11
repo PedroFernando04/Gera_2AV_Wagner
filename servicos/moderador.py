16b8b25f5aabe13490c2ca5b4ac442c954744260
PseravaT <163481412+PseravaT@users.noreply.github.com>
2024-11-11 11:44:37 -0300

    Update final moderador.py

--- servicos/moderador.py
+++ servicos/moderador.py
@@ -1,3 +1,55 @@
+def visualizar_itens(conn):
+
+    try:
+        with conn.cursor() as cursor:
+            query = "SELECT * FROM gera.itens"
+            cursor.execute(query)
+            registros = cursor.fetchall()
+            if registros:
+                print("\nTipo: Itens")
+                print("-" * 120)
+                for id_item, nome, id_class_item, quantidade, valor, peso in registros:
+                    print(f"ID: {id_item}, NOME: {nome}, CLASSIFICAÇÃO: {id_class_item}, QUANTIDADE: {quantidade}, VALOR: {valor}, PESO: {peso}")
+                print("-" * 120)
+            else:
+                print("Nenhum item encontrado.")
+    except Exception as e:
+        print(e)
+
+def visualizar_armaduras(conn):
+    try:
+        with conn.cursor() as cursor:
+            query = "SELECT * FROM gera.armaduras"
+            cursor.execute(query)
+            registros = cursor.fetchall()
+            if registros:
+                print("\nTipo: Armaduras")
+                print("-" * 120)
+                for id_armadura, nome, tipo_armadura, bonus, defesa_base, valor, peso in registros:
+                    print(f"ID: {id_armadura}, NOME: {nome}, TIPO: {tipo_armadura}, BÔNUS: {bonus}, DEFESA BASE: {defesa_base}, VALOR: {valor}, PESO: {peso}")
+                print("-" * 120)
+            else:
+                print("Nenhuma armadura encontrada.")
+    except Exception as e:
+        print(e)
+
+def visualizar_armas(conn):
+    try:
+        with conn.cursor() as cursor:
+            query = "SELECT * FROM gera.armas"
+            cursor.execute(query)
+            registros = cursor.fetchall()
+            if registros:
+                print("\nTipo: Armas")
+                print("-" * 120)
+                for id_arma, nome, tipo_arma, bonus, ataque_base, valor, peso in registros:
+                    print(f"ID: {id_arma}, NOME: {nome}, TIPO: {tipo_arma}, BÔNUS: {bonus}, ATAQUE BASE: {ataque_base}, VALOR: {valor}, PESO: {peso}")
+                print("-" * 120)
+            else:
+                print("Nenhuma arma encontrada.")
+    except Exception as e:
+        print(e)
+
 def classifcacao_itens(conn):
     try:
         with conn.cursor() as cursor:
@@ -11,7 +63,7 @@ def classifcacao_itens(conn):
                     print(f"ID: {id_class_item}, CATEGORIA: {categoria}, TIPO: {tipo}, EFEITO: {efeito}")
                 print("-" * 120)
             else:
-                print("Não foi possível encontrar resultados para sua busca.")
+                print("nenhuma ")
     except Exception as e:
         print(e)
 
@@ -24,53 +76,24 @@ def visualizar_dados(conn):
         print("3 - Armas")
         print("4 - Classificação de itens")
         print("5 - Sair")
-        tabela = int(input("Escolha uma tabela: "))
+        opc = int(input("Escolha uma tabela: "))
         
-        match tabela:
+        match opc:
             case 1:
-                tabela = "itens"
-                tipo = "Itens"
+                visualizar_itens(conn)
             case 2:
-                tabela = "armaduras"
-                tipo = "Armaduras"
+                visualizar_armaduras(conn)
             case 3:
-                tabela = "armas"
-                tipo = "Armas"
+                visualizar_armas(conn)
             case 4:
                 classifcacao_itens(conn)
                 continue
             case 5:
-                print("\nSaindo do menu Visualizar...\n")
+                print("\nSaindo do menu de Visualização...\n")
                 break
             case _:
                 print("Opção inválida.")
                 continue
-
-        try:
-            with conn.cursor() as cursor:
-                query = f"SELECT * FROM gera.{tabela};"
-                cursor.execute(query)
-                registros = cursor.fetchall()
-                if registros:
-                    print(f"\nTipo: {tipo}")
-                    print("-" * 120)
-
-                    if tipo == "Itens":
-                        for id_item, nome, id_class_item, quantidade, valor, peso in registros:
-                            print(f"ID: {id_item}, NOME: {nome}, CLASSIFICAÇÃO: {id_class_item}, QUANTIDADE: {quantidade}, VALOR: {valor}, PESO: {peso}")
-
-                    elif tipo == "Armaduras":
-                        for id_armadura, nome, tipo_armadura, bonus, defesa_base, valor, peso in registros:
-                            print(f"ID: {id_armadura}, NOME: {nome}, TIPO: {tipo_armadura}, BÔNUS: {bonus}, DEFESA BASE: {defesa_base}, VALOR: {valor}, PESO: {peso}")
-
-                    elif tipo == "Armas":
-                        for id_arma, nome, tipo_arma, bonus, ataque_base, valor, peso in registros:
-                            print(f"ID: {id_arma}, NOME: {nome}, TIPO: {tipo_arma}, BÔNUS: {bonus}, ATAQUE BASE: {ataque_base}, VALOR: {valor}, PESO: {peso}")
-                else:
-                    print("Nenhum registro encontrado.")
-            print("-" * 120)
-        except Exception as e:
-            print(e)
 
 
 def inserir_dados(conn):
@@ -80,9 +103,9 @@ def inserir_dados(conn):
         print("2 - Armaduras")
         print("3 - Armas")
         print("4 - Sair...")
-        tabela = int(input("Escolha uma tabela: "))
+        opc = int(input("Escolha uma tabela: "))
         
-        match tabela:
+        match opc:
             case 1:
                 classifcacao_itens(conn)
                 tabela = 'itens'
@@ -115,7 +138,7 @@ def inserir_dados(conn):
                 valores = (nome, tipo, bonus, ataque_base, valor, peso)
                 query = f"INSERT INTO gera.{tabela} (nome, tipo, bonus, ataque_base, valor, peso) VALUES (%s, %s, %s, %s, %s, %s)"
             case 4:
-                print("Saindo...")
+                print("Saindo do menu de Inserção...\n")
                 break
 
         try:
@@ -125,3 +148,64 @@ def inserir_dados(conn):
                 print(f"{nome} inserido com sucesso na tabela {tabela}!")
         except Exception as e:
             print(e)
+
+def alterar_dados(conn):
+    while True:
+        print("\nAlterar em:")
+        print("1 - Itens")
+        print("2 - Armaduras")
+        print("3 - Armas")
+        print("4 - Sair...")
+        opc = int(input("Escolha uma tabela: "))
+        
+        match opc:
+            case 1:
+                visualizar_itens(conn)
+                classifcacao_itens(conn)
+                tabela = "Itens"
+                item_id = int(input(f"Informe o ID do item que deseja alterar na tabela itens: "))
+                nome = input("Novo nome do item: ")
+                id_class_item = int(input("Nova classificação do item (ID): "))
+                quantidade = int(input("Nova quantidade: "))
+                valor = float(input("Novo valor: "))
+                peso = float(input("Novo peso: "))
+                query = f"UPDATE gera.itens SET nome = %s, id_class_item = %s, quantidade = %s, valor = %s, peso = %s WHERE id_item = %s"
+                valores = (nome, id_class_item, quantidade, valor, peso, item_id)
+            case 2:
+                visualizar_armaduras(conn)
+                tabela = "Armaduras"
+                item_id = int(input(f"Informe o ID do item que deseja alterar na tabela Armaduras: "))
+                nome = input("Novo nome do item: ")
+                id_class_item = int(input("Nova classificação do item (ID): "))
+                quantidade = int(input("Nova quantidade: "))
+                valor = float(input("Novo valor: "))
+                peso = float(input("Novo peso: "))
+                query = f"UPDATE gera.armaduras SET nome = %s, id_class_item = %s, quantidade = %s, valor = %s, peso = %s WHERE id_armadura = %s"
+                valores = (nome, id_class_item, quantidade, valor, peso, item_id)
+            case 3:
+                visualizar_armas(conn)
+                tabela = "Armas"
+                item_id = int(input(f"Informe o ID do item que deseja alterar na tabela Armas: "))
+                nome = input("Novo nome da arma: ")
+                tipo = input("Novo tipo: ")
+                bonus = input("Novo bônus: ")
+                ataque_base = int(input("Novo ataque base: "))
+                valor = float(input("Novo valor: "))
+                peso = float(input("Novo peso: "))
+                query = f"UPDATE gera.armas SET nome = %s, tipo = %s, bonus = %s, ataque_base = %s, valor = %s, peso = %s WHERE id_arma = %s"
+                valores = (nome, tipo, bonus, ataque_base, valor, peso, item_id)
+            case 4:
+                print("Saindo do menu de alteração...\n")
+                break
+            case _:
+                print("Opção inválida.")
+                continue   
+        try:       
+            with conn.cursor() as cursor:
+                cursor.execute(query, valores)
+                conn.commit()
+                print(f"{nome} atualizado com sucesso na tabela {tabela}!")
+        
+        except Exception as e:
+            print(e)
+
