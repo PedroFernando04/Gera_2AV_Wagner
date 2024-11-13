feb73ccc08328d732a787f0d389efcf06249c053
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-13 17:22:21 -0300

    Update CadLog.py

--- FUNCTIONS/Login/Cadastro_login/CadLog.py
+++ FUNCTIONS/Login/Cadastro_login/CadLog.py
@@ -13,27 +13,32 @@ def cadastrar (conn):
     os.system('cls' or 'clear')
 
     print('\n[ CADASTRO ]\n')
-    email = emailValido(usuarios)
+    email = emailValido(usuarios, conn)
     senha = senhaValida()
     
     #colocar na lista do gera
     usuarios.append(Cadastro(email,senha))
 
     #colocar no bd
-    cursor = conn.cursor()
-    query = f"INSERT INTO gera.usuarios(email, senha) VALUES (\'{email}\', \'{senha}\')"
-    cursor.execute(query)
-    return usuarios
+    try:
+        cursor = conn.cursor()
+        query = f"INSERT INTO gera.usuarios(email, senha, mod, id_personagem) VALUES ('{email}', '{senha}', 'false' , '1')"
+        cursor.execute(query)
+        conn.commit()
+        return usuarios
+    except Exception as e:
+        print(e)
+        input()
 
 
-def logar (users):
+def logar (users, conn):
     os.system('cls' or 'clear')
     
     print('\n[ LOGIN ]\n')
     nome_login = input ('Digite seu Email: ')
     senha_login = input ('Digite sua senha: ')
 
-    if loginValido(nome_login, senha_login, users):
+    if loginValido(nome_login, senha_login, users, conn):
         return True
     else:
         print('\nUsuário e senha inválida!')
