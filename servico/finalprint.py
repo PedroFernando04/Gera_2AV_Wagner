11dabe1a7f785c8da1937c29c299fcec9a1af67a
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-22 17:42:33 -0300

    Update finalprint.py

--- servico/finalprint.py
+++ servico/finalprint.py
@@ -1,9 +1,8 @@
 import os
-
 def print_final (conn, id_personagem):
     try:
         with conn.cursor () as cursor:
-            query = """select p.nome, p.vida, p.dinheiro, p.classe,r.nome as raca,it.nome as item,def.nome as armadura, atc.nome as arma,forca,destreza, constituicao,inteligencia,sabedoria,carisma
+            query = f"""select p.nome, p.vida, p.dinheiro, p.classe,r.nome as raca,it.nome as item,def.nome as armadura, atc.nome as arma,forca,destreza, constituicao,inteligencia,sabedoria,carisma
                         from gera.personagens p 
                         join gera.atributos atb on atb.id_atributo = p.id_atributo
                         join gera.racas r on r.id_raca = p.id_raca
@@ -11,16 +10,18 @@ def print_final (conn, id_personagem):
                         left join gera.itens it on it.id_item  = inv.id_item
                         left join gera.armaduras def on def.id_armadura = inv.id_armadura
                         left join gera.armas atc on atc.id_arma = inv.id_arma
-                        where id_personagem = %s
+                        where id_personagem = {id_personagem}
                         """
-            cursor.execute(query,(id_personagem,))
+            cursor.execute(query)
             lista_perso= ["Nome", "Vida", "Dinheiro", "Classe", "Raça", "Item", "Armadura", "Arma"]
             lista_atributos = ["Força", "Destreza", "Constituição", "Inteligência", "Sabedoria", "Carisma"]
             valores = cursor.fetchall ()
-            
+
+            os.system('cls' or 'clear')
+            print("-" * 120)
+
             for linha in valores:
-                os.system('cls' or 'clear')
-                print("-" * 120)
+                
                 for i in range(0,len(lista_perso)):
                     print(f"{lista_perso[i]}: {linha[i]}")
                 
